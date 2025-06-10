fc2be98c852871c212ffaebbba5f3658eb9fe14b
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2025-06-10 11:38:42 -0700

    added contact about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,56 @@
+import CustomImage from "../../components/CustomImage/CustomImage";
+import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
+
+const aboutImages = [
+  {
+    imgSrc: "/images/home_images/issy_home.JPG",
+    imgAlt: "Isioma Idehen in a studio setting",
+    imgWidth: 2400,
+    imgHeight: 3600,
+    containerClassname: "max-h-[50vh] sm:max-h-[70vh] flex",
+  },
+  {
+    imgSrc: "/images/home_images/issy_home.JPG",
+    imgAlt: "Isioma Idehen in a creative workspace",
+    imgWidth: 2400,
+    imgHeight: 3600,
+    containerClassname: "max-h-[50vh] sm:max-h-[70vh] flex",
+  },
+];
 const About = () => {
-  return <div>This is the about page</div>;
+  return (
+    <SectionWrapper title="About">
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-16">
+        <p>
+          Isioma Idehen is a director, creative director and writer passionate
+          about telling emotive and raw stories across different mediums. With a
+          background in fashion textiles and years spent as a cinema host ion my
+          late teens. I've been able to merge my love for fashion and film to
+          create visually appealing narratives
+        </p>
+        <p>
+          Isioma Idehen is a director, creative director and writer passionate
+          about telling emotive and raw stories across different mediums. With a
+          background in fashion textiles and years spent as a cinema host ion my
+          late teens. I've been able to merge my love for fashion and film to
+          create visually appealing narratives
+        </p>
+      </section>
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-16">
+        <div></div>
+        {aboutImages.map((image, index) => (
+          <CustomImage
+            key={index}
+            imgSrc={image.imgSrc}
+            imgAlt={image.imgAlt}
+            width={image.imgWidth}
+            height={image.imgHeight}
+            classname={image.containerClassname}
+          />
+        ))}
+      </section>
+    </SectionWrapper>
+  );
 };
 
 export default About;
